bb3264adfff8e46626113fb3229bc2dd9516e80b
HP 250g7 <brightology2@gmain.com>
2025-02-16 21:42:26 +0100

    Fetching Quiz From DB

--- src/main/java/com/brightology/quizapp/controller/QuizController.java
+++ src/main/java/com/brightology/quizapp/controller/QuizController.java
@@ -1,10 +1,14 @@
 package com.brightology.quizapp.controller;
 
+import com.brightology.quizapp.model.Question;
+import com.brightology.quizapp.model.QuestionWrapper;
 import com.brightology.quizapp.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/quiz")
@@ -16,5 +20,11 @@ public class QuizController {
     @PostMapping("/create")
     public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title){
         return quizService.createQuiz(category, numQ, title);
+    }
+
+    @GetMapping("/getquiz/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id){
+        return quizService.getQuizQuestions(id);
+
     }
 }
--- src/main/java/com/brightology/quizapp/service/QuizService.java
+++ src/main/java/com/brightology/quizapp/service/QuizService.java
@@ -3,6 +3,7 @@ package com.brightology.quizapp.service;
 import com.brightology.quizapp.dao.QuestionDao;
 import com.brightology.quizapp.dao.QuizDao;
 import com.brightology.quizapp.model.Question;
+import com.brightology.quizapp.model.QuestionWrapper;
 import com.brightology.quizapp.model.Quiz;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,7 +11,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuizService {
@@ -31,5 +34,18 @@ public class QuizService {
         quizDao.save(quiz);
 
         return new ResponseEntity<>("Quiz created successfully", HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
+
+        Optional<Quiz> quiz = quizDao.findById(id);
+        List<Question> questionsFromDB = quiz.get().getQuestions();
+        List<QuestionWrapper> questionsForUser = new ArrayList<>();
+        for (Question q : questionsFromDB) {
+             QuestionWrapper qw = new QuestionWrapper(q.getId(), q.getQuestionTitle(), q.getOption1(), q.getOption2(), q.getOption3(), q.getOption4());
+                questionsForUser.add(qw);
+        }
+
+        return new ResponseEntity<>(questionsForUser, HttpStatus.OK);
     }
 }
