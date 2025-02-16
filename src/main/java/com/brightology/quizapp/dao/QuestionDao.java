b23a3fe84e9e8798f6044ba56557dfe394337ed0
HP 250g7 <brightology2@gmain.com>
2025-02-16 10:45:02 +0100

    Quiz Creation

--- src/main/java/com/brightology/quizapp/dao/QuestionDao.java
+++ src/main/java/com/brightology/quizapp/dao/QuestionDao.java
@@ -2,7 +2,9 @@ package com.brightology.quizapp.dao;
 
 import com.brightology.quizapp.model.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -10,4 +12,8 @@ import java.util.List;
 public interface QuestionDao extends JpaRepository<Question, Integer> {
 
     List<Question> findAllByCategory(String category);
+
+    @Query(value ="SELECT * FROM question q WHERE q.category=:category ORDER BY RANDOM() LIMIT :numQ", nativeQuery = true)
+    List<Question> findRandomQuestionByCategory(String category, int numQ);
+
 }
---
+++ src/main/java/com/brightology/quizapp/service/QuizService.java
@@ -0,0 +1,35 @@
+package com.brightology.quizapp.service;
+
+import com.brightology.quizapp.dao.QuestionDao;
+import com.brightology.quizapp.dao.QuizDao;
+import com.brightology.quizapp.model.Question;
+import com.brightology.quizapp.model.Quiz;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Service
+public class QuizService {
+
+    @Autowired
+    QuizDao quizDao;
+
+    @Autowired
+    QuestionDao questionDao;
+
+    public ResponseEntity<String> createQuiz(String category, int numQ, String title) {
+
+        List<Question> questions = questionDao.findRandomQuestionByCategory(category, numQ);
+
+        Quiz quiz = new Quiz();
+        quiz.setTitle(title);
+        quiz.setQuestions(questions);
+        quizDao.save(quiz);
+
+        return new ResponseEntity<>("Quiz created successfully", HttpStatus.CREATED);
+    }
+}
